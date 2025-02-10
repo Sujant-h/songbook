b39947861794aab284ea6acd2f2c007dd80fe830
sujanth <sujanth.suthaharan@hm.edu>
2025-02-10 23:35:43 +0100

    Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
-
+  site: 'https://sujat-h.github.io',
+  base: 'astro-songbook',
   vite: {
     plugins: [tailwindcss()]
   }
