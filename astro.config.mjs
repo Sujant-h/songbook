95e48400ca4aea29c968455415f61f4675ed6970
sujanth <sujanth.suthaharan@hm.edu>
2025-02-10 23:58:27 +0100

    Rename site path and base path in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://sujant-h.github.io/astro-songbook/',  // Full URL with trailing slash
-  base: '/astro-songbook/',                            // Leading and trailing slashes
+  site: 'https://sujant-h.github.io/songbook/',  // Full URL with trailing slash
+  base: '/songbook/',                            // Leading and trailing slashes
   trailingSlash: 'always',                             // Ensure all URLs end with a trailing slash
   vite: {
     plugins: [tailwindcss()]
