469dcd901b1262e2c5fc4839b8b3dfd65894cbb0
Sujanth <84275282+Sujant-h@users.noreply.github.com>
2025-02-27 02:34:17 +0100

    Fix theme toggle function v3 | make assets avaible

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ export default defineConfig({
   site: 'https://sujant-h.github.io',
   base: 'songbook',
   trailingSlash: 'ignore',
+  build: {
+    // Makes assets available at the correct path
+    assets: '_assets',
+  },
   vite: {
     logLevel: 'info',
     define: {
